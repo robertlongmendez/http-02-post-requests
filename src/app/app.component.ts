f88917af88181ab5f51741cb10481f6d1b645da7
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-07-28 14:46:29 -0500

    added fetch post functionality with firebase as real time database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +10,19 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+      .post<{ name: string }>(
+        'https://ng-complete-guide-3747e-default-rtdb.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
@@ -27,9 +32,29 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-3747e-default-rtdb.firebaseio.com/posts.json')
+      .pipe(
+        map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key })
+          }
+        }
+        return postsArray;
+      })
+      )
+      .subscribe(posts => {
+        this.loadedPosts = posts;
+      });
   }
 }
